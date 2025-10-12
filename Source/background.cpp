89ae51e7ead0dd3cd8caf635eb10355f2a6d1ebf
春翔 木村 <harkimura7@yahoo.ne.jp>
2025-10-12 21:30:19 +0900

    ロゴの追加

--- Source/background.cpp
+++ Source/background.cpp
@@ -7,7 +7,7 @@ BackGround::BackGround()
 {
 	wImage = LoadGraph("data/image/white.jpg");
 	assert(wImage > 0);
-	rImage = LoadGraph("data/image/red.jpg");
+	rImage = LoadGraph("data/image/Rogo.png");
 	assert(rImage > 0);
 	hImage = LoadGraph("data/image/skin color.jpg");
 	assert(hImage > 0);
@@ -31,6 +31,7 @@ void BackGround::Draw()
 {
 	//DrawExtendGraph(0, 0, 160, 1080, hImage, 0);// ç∂í[
 	DrawExtendGraph(1360, 72, 1920, 765, hImage, 0);//âEí[
-	DrawExtendGraph(0, 0, 1920, 72, rImage, 0);//è„
+	DrawExtendGraph(0, 0, 1920, 72, wImage, 0);//è„
+	DrawExtendGraph(0, 0, 1920, 72, rImage, 0);//ÉçÉS
 	DrawExtendGraph(0, 765, 1920, 1080, wImage, 0);//â∫
 }
