e84f520d2386ca2c5f62ccfd3e8058a33b4e18fa
240023 <240023@E0403-36>
2025-10-14 16:36:58 +0900

    ゲームオーバーの追加

--- Source/RetryScene.cpp
+++ Source/RetryScene.cpp
@@ -15,6 +15,9 @@ void RetryScene::Update()
 	if (CheckHitKey(KEY_INPUT_P)) {
 		SceneManager::ChangeScene("PLAY");
 	}
+	if (CheckHitKey(KEY_INPUT_G)) {
+		SceneManager::ChangeScene("GAMEOVER");
+	}
 	if (CheckHitKey(KEY_INPUT_ESCAPE)) {
 		SceneManager::Exit();
 	}
@@ -24,4 +27,5 @@ void RetryScene::Draw()
 {
 	DrawString(0, 0, "RETRY SCENE", GetColor(255, 255, 255));
 	DrawString(100, 400, "Push [P]Key To Play", GetColor(255, 255, 255));
+	DrawString(100, 500, "Push [G]Key To GameOver", GetColor(255, 255, 255));
 }
